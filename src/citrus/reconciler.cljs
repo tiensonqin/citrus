29d080a386857d68f903b6552752557cda110285
Tienson Qin <tiensonqin@gmail.com>
2018-01-09 13:42:00 +0800

    fixed issue

--- src/citrus/reconciler.cljs
+++ src/citrus/reconciler.cljs
@@ -39,9 +39,9 @@
   IWatchable
   (-add-watch [this key callback]
     (add-watch state (list this key)
-      (fn [_ _ oldv newv]
-        (when (not= oldv newv)
-          (callback key this oldv newv))))
+               (fn [_ _ oldv newv]
+                 (when (not= oldv newv)
+                   (callback key this oldv newv))))
     this)
 
   (-remove-watch [this key]
@@ -59,48 +59,48 @@
 
   IReconciler
   (dispatch! [this event args]
-    (let [ctrl (keyword (namespace event))
-          seperate-state? (not= ctrl :citrus)]
+    (let [cname (keyword (namespace event))
+          seperate-state? (not= cname :citrus)]
       (queue-effects!
        queue
-       #(apply (get handler event)
-          (if seperate-state? (get @state ctrl) @state)
-          args))
+       [cname #(apply (get handler event)
+                 (if seperate-state? (get % cname) %)
+                 args)])
 
       (schedule-update!
        batched-updates
        scheduled?
        (fn []
-         (let [effects
-               (map (fn [f] (f)) @queue)]
-           (clear-queue! queue)
-           (let [state-effects (filter :state effects)
-                 other-effects (->> effects
-                                    (map (fn [effect]
-                                           (dissoc effect :state)))
-                                    (filter seq))]
-             (when (seq state-effects)
-               (swap! state #(reduce (fn [agg {cstate :state}]
-                                       (if seperate-state?
-                                         (update agg ctrl merge cstate)
-                                         (merge agg cstate)))
-                                     % state-effects)))
-             (when (seq other-effects)
-               (m/doseq [effects effects]
-                 (m/doseq [[id effect] effects]
-                   (when-let [handler (get effect-handlers id)]
-                     (handler this effect)))))))))))
+         (let [events @queue
+               _ (clear-queue! queue)
+               next-state
+               (loop [st @state
+                      [event & events] events]
+                 (if (seq event)
+                   (let [[cname ctrl] event
+                         effects (ctrl st)]
+                     (m/doseq [[id effect] (dissoc effects :state)]
+                       (when-let [handler (get @effect-handlers id)]
+                         (handler this effect)))
+                     (if (contains? effects :state)
+                       (let [seperate-state? (not= cname :citrus)]
+                         (recur (if seperate-state?
+                                  (update st cname merge (:state effects))
+                                  (merge st (:state effects))) events))
+                       (recur st events)))
+                   st))]
+           (reset! state next-state))))))
 
   (dispatch-sync! [this event args]
     (let [effects (apply (get handler event) @state args)
-          ctrl (keyword (namespace event))
-          seperate-state? (not= ctrl :citrus)]
+          cname (keyword (namespace event))
+          seperate-state? (not= cname :citrus)]
       (m/doseq [[id effect] effects]
-        (let [handler (get effect-handlers id)]
+        (let [handler (get @effect-handlers id)]
           (cond
             (= id :state) (swap! state (fn [state]
                                          (if seperate-state?
-                                           (update state ctrl merge effect)
+                                           (update state cname merge effect)
                                            (merge state effect))))
             handler (handler this effect)
             :else nil))))))
